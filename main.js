085fde44c238129a309826febf355e4a648237b5
Oluchukwu Divine <oluchukwudivine31@gmail.com>
2023-03-29 21:01:48 +0100

    Done

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("submitbtn").addEventListener("click", function(e){
+    const name = document.getElementById("username").value;
+    const email= document.getElementById("email").value;
+    const phone= document.getElementById("number").value;
+    const password= document.getElementById("password").value;
+
+    if(name==="" || email==="" || phone==="" || password===""){
+        alert("Please fill in all fields");
+        e.preventDefault();
+    } else{
+        alert("Form has been submitted successfully");
+    }
+
+}
+
+)
